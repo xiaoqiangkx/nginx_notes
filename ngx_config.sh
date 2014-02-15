14cc027154de08d8d53528fc0f4ffdc3428975f2
xiaoqiangkx <xiaoqiangkx@gmail.com>
2014-02-15 19:48:26 +0800

    add basic_types_int

--- ngx_config.sh
+++ ngx_config.sh
@@ -2,4 +2,5 @@ alias croot='cd ~/learning/nginx'
 alias cngx='cd ~/learning/nginx/nginx'
 alias ccore='cd ~/learning/nginx/nginx/src/core'
 alias cds='cd ~/learning/nginx/data_structure'
+alias csrc='cd ~/learning/nginx/nginx/src/'
 
